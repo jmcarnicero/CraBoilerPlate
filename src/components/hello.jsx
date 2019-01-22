c233412274b69303601cbddf5b6098269ed1bcf1
Pepe Carnicero <you@example.com>
2019-01-22 11:52:30 +0100

    initial commit

---
+++ src/components/hello.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Hello = () => <div className="App">Helloooooooooooooooo</div>;
+
+export default Hello;
---
+++ src/temp/helloTemp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import 'App.css';
+
+const HelloTemp = () => <div className="App">HelloTemp</div>;
+
+export default HelloTemp;
